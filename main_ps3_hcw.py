f0283921118d8898632e38db91658ba69d8188ed
sammmlow <sammmlow@gmail.com>
2023-04-21 19:30:47 -0700

    PS3 completed HCW STM propagation

--- main_ps3_hcw.py
+++ main_ps3_hcw.py
@@ -11,6 +11,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import sin, cos, pi
 
+# Import our own spacecraft library
+from source import spacecraft
+
 ##############################################################################
 ##############################################################################
 
@@ -75,7 +78,7 @@ def stm_hcw(sc, t):
 ##############################################################################
 ##############################################################################
 
-from main_ps2 import get_hill_frame, rv_eci_to_rtn, accel_rtn, relative_rk4
+from main_ps2 import rv_eci_to_rtn, relative_rk4
 
 # Now test and see, based on initial conditions in Table 2 of PS3, if HCW
 # approximates motion well, with the non-linear FDERM propagation.
@@ -85,10 +88,11 @@ sc2_elements = [6929.137, 0.000001, 97.5976, 0.0, 250.6703, 0.00413]
 # Create the spacecraft objects.
 sc1 = spacecraft.Spacecraft( elements = sc1_elements )
 sc2 = spacecraft.Spacecraft( elements = sc2_elements )
+sc2 = spacecraft.Spacecraft( elements = sc2_elements )
 
 # Start the simulation here.
 timeNow, duration, timestep = 0.0, 86400.0, 30.0 # Seconds
-samples = int(duration / timestep) * 3
+samples = int(duration / timestep)
 k = 0  # Sample count
 
 # Matrix to store the data
@@ -100,20 +104,33 @@ rv_c_eci = np.array([sc1.px, sc1.py, sc1.pz, sc1.vx, sc1.vy, sc1.vz])
 rv_d_eci = np.array([sc2.px, sc2.py, sc2.pz, sc2.vx, sc2.vy, sc2.vz])
 rv_cd_eci = rv_d_eci - rv_c_eci
 r_rtn, v_rtn = rv_eci_to_rtn(rv_c_eci, rv_cd_eci)
+rv_rtn = np.concatenate((r_rtn, v_rtn)) # Merge the pos and vel
 
 # Make 2 copies of the initial RTN states (one for HCW, and one for truth)
-r_rtn_hcw,  v_rtn_hcw  = r_rtn, v_rtn
-r_rtn_true, v_rtn_true = r_rtn, v_rtn
+rv_rtn_hcw = rv_rtn
+rv_rtn_true = rv_rtn
 
+# ACTUAL SIMULATION CODE BELOW. Note: the actual SC2 object isn't used below.
 while timeNow < duration:
 
-    # Record states for SC1 (using HCW state transitions)
-    rtn_states_hcw[n, 0:3] = r_rtn_hcw
-    rtn_states_hcw[n, 3:6] = v_rtn_hcw
+    # Record states for SC2 copy (using HCW state transitions)
+    rtn_states_hcw[k,:] = rv_rtn_hcw
     
-    # Record states for SC2 (using non-linear FDERM)
-    rtn_states_true[n, 0:3] = r_rtn_true
-    rtn_states_true[n, 3:6] = v_rtn_true
+    # Record states for SC2 copy (using non-linear FDERM)
+    rtn_states_true[k,:] = rv_rtn_true
+    
+    # Propagate states for SC2 copy (using HCW state transitions)
+    rv_rtn_hcw = stm_hcw(sc1, timestep) @ rv_rtn_hcw
+    
+    # Propagate states for SC2 copy (using non-linear FDERM)
+    rv_rtn_true[0:3], rv_rtn_true[3:6] = relative_rk4(timestep, sc1.n,
+                                                      rv_rtn_true[0:3],
+                                                      rv_rtn_true[3:6], sc1.a)
+    
+    print(np.linalg.norm(rv_rtn_true - rv_rtn_hcw))
+    
+    # Finally, the chief itself needs to be propagated (in absolute motion)
+    sc1.propagate_perturbed(timestep, timestep)
 
     # Update time and sample count.
     timeNow += timestep
@@ -122,58 +139,57 @@ while timeNow < duration:
 ##############################################################################
 ##############################################################################
 
-# HCW state transition matrix.
-# Inputs are a `Spacecraft` object and time of propagation [sec].
-def stm_ya(sc, t):
-    
-    stm = np.zeros((6,6))
-    
-    # First row
-    stm[0,0] = 
-    stm[0,1] = 
-    stm[0,2] = 
-    stm[0,3] = 
-    stm[0,4] = 
-    stm[0,5] = 
-    
-    # Second row
-    stm[1,0] = 
-    stm[1,1] = 
-    stm[1,2] = 
-    stm[1,3] = 
-    stm[1,4] = 
-    stm[1,5] = 
-    
-    # Third row
-    stm[2,0] = 
-    stm[2,1] = 
-    stm[2,2] = 
-    stm[2,3] = 
-    stm[2,4] = 
-    stm[2,5] = 
-    
-    # Fourth row
-    stm[3,0] = 
-    stm[3,1] = 
-    stm[3,2] = 
-    stm[3,3] = 
-    stm[3,4] = 
-    stm[3,5] = 
-    
-    # Fifth row
-    stm[4,0] = 
-    stm[4,1] = 
-    stm[4,2] = 
-    stm[4,3] = 
-    stm[4,4] = 
-    stm[4,5] = 
-    
-    # Sixth row
-    stm[5,0] = 
-    stm[5,1] = 
-    stm[5,2] = 
-    stm[5,3] = 
-    stm[5,4] = 
-    stm[5,5] = 
-    
-    return stm
+plt.close('all')
+timeAxis = np.linspace(0, duration, samples)
+
+
+# Plot position errors between HCW and truth
+plt.figure(1)
+
+plt.subplot(3, 1, 1)
+plt.title('`HCW` minus `Truth` position error in RTN [km]')
+plt.plot(timeAxis, rtn_states_true[:, 0] - rtn_states_hcw[:, 0])
+plt.grid()
+plt.xlabel('Simulation time [sec]')
+plt.ylabel('R component [km]')
+
+plt.subplot(3, 1, 2)
+plt.plot(timeAxis, rtn_states_true[:, 1] - rtn_states_hcw[:, 1])
+plt.grid()
+plt.xlabel('Simulation time [sec]')
+plt.ylabel('T component [km]')
+
+plt.subplot(3, 1, 3)
+plt.plot(timeAxis, rtn_states_true[:, 2] - rtn_states_hcw[:, 2])
+plt.grid()
+plt.xlabel('Simulation time [sec]')
+plt.ylabel('N component [km]')
+
+plt.show()
+
+###############################################################################
+###############################################################################
+
+# Plot velocity errors between HCW and truth
+plt.figure(2)
+
+plt.subplot(3, 1, 1)
+plt.title('`HCW` minus `Truth` velocity error in RTN [km/s]')
+plt.plot(timeAxis, rtn_states_true[:, 3] - rtn_states_hcw[:, 3])
+plt.grid()
+plt.xlabel('Simulation time [sec]')
+plt.ylabel('R component [km/s]')
+
+plt.subplot(3, 1, 2)
+plt.plot(timeAxis, rtn_states_true[:, 4] - rtn_states_hcw[:, 4])
+plt.grid()
+plt.xlabel('Simulation time [sec]')
+plt.ylabel('T component [km/s]')
+
+plt.subplot(3, 1, 3)
+plt.plot(timeAxis, rtn_states_true[:, 5] - rtn_states_hcw[:, 5])
+plt.grid()
+plt.xlabel('Simulation time [sec]')
+plt.ylabel('N component [km/s]')
+
+plt.show()
